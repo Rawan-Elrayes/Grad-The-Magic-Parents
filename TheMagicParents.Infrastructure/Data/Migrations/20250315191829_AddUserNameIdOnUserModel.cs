c44d0467af63ee281e4110354fad43bb5b34d8d8
Rawan-Elrayes <shrienhemed@gmail.com>
2025-04-24 14:53:59 +0200

    endFirstPart

---
+++ TheMagicParents.Infrastructure/Data/Migrations/20250315191829_AddUserNameIdOnUserModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheMagicParents.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserNameIdOnUserModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserNameId",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserNameId",
+                table: "AspNetUsers");
+        }
+    }
+}
